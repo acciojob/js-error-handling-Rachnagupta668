c8d370445b29a414222f2fb78265396ba5135974
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-05-04 09:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
-
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
 //your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+let input = document.getElementById("input").value;
+
+class OutOfRangeError extends Error{
+	constructor(arg) {
+		super(`Expression should only consist of integers and +-/* characters and not ${arg}`)
+	    this.name = "OutOfRangeError";
+	}
+}
+
+class InvalidExprError extends Error{
+	constructor() {
+		super(`Expression should not have an invalid combination of expression`)
+	    this.name = "InvalidExprError";
+	}
+}
